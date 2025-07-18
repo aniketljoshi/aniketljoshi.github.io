9461f7ea0099dd668b13717f5a8af0880269baba
Aniket Joshi <59261142+aniketljoshi@users.noreply.github.com>
2025-07-18 08:46:41 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ExperienceComponent } from './experience/experience.component';
+import { SummaryComponent } from './summary/summary.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AchievementsDomainsComponent } from './achievements-domains/achievements-domains.component';
+
+const routes: Routes = [
+  { path: 'summary', component: SummaryComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'achievements', component: AchievementsDomainsComponent },
+  { path: '', redirectTo: '/summary', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
