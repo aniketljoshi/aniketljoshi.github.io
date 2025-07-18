2d1397258b2567f1ecdd032a0031bd08fe50fe9b
Aniket Joshi <59261142+aniketljoshi@users.noreply.github.com>
2025-07-18 08:44:41 +0530

    Add files via upload

---
+++ src/app/achievements-domains/achievements-domains.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-achievements-domains',
+  templateUrl: './achievements-domains.component.html',
+  styleUrls: ['./achievements-domains.component.css']
+})
+export class AchievementsdomainsComponent { }
