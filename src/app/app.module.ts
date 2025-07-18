a40ec349aafe3fedd08b8342fce1a28555c3ce3f
Aniket Joshi <59261142+aniketljoshi@users.noreply.github.com>
2025-07-18 08:31:59 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { ExperienceComponent } from './experience/experience.component';
+import { SummaryComponent } from './summary/summary.component';
+import { SkillsComponent } from './skills/skills.component';
+import { AchievementsDomainsComponent } from './achievements-domains/achievements-domains.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ExperienceComponent,
+    SummaryComponent,
+    SkillsComponent,
+    AchievementsDomainsComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
+  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
